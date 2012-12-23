b81fc1e3afbd751d191d753a0eb087a17ac7a9fb
Nick Sagona <nick@moc10media.com>
2012-12-23 00:56:58 -0600

    Work on the CLI features

--- script/pop.bat
+++ script/pop.bat
@@ -1,5 +1,13 @@
 @echo off
 
-SET SCRIPT_DIR=%~dp0
-php %SCRIPT_DIR%pop.php %*
+SET TAR=1
+SET ZIP=1
+FOR /f "delims=" %%i in ('where tar') do set TAR=%%i
+FOR /f "delims=" %%i in ('where zip') do set ZIP=%%i
+
+REM echo %TAR%
+REM echo %ZIP%
+
+REM SET SCRIPT_DIR=%~dp0
+REM php %SCRIPT_DIR%pop.php %*
 
