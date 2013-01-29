596c844e6a71ba04703fd90679a0003b21ae3516
Nick Sagona <nick@moc10media.com>
2013-01-29 16:41:18 -0600

    Revise/tweak pop.bat CLI script

--- script/pop.bat
+++ script/pop.bat
@@ -32,13 +32,15 @@ IF "%1" == "install" (
             SET TEMPFILE=%2%
             SET TESTFILE=..\vendor\Popcorn\src\Pop\%TEMPFILE%%EXT%
         )
-         IF EXIST "%TESTFILE%" (
+        IF EXIST "%TESTFILE%" (
             FOR /f "delims=" %%i IN ('dir /B ..\vendor\Popcorn\src\Pop\*%EXT%') DO (
                 echo Unpacking %%i...
                 %CMD% ../vendor/Popcorn/src/Pop/%%i
                 del ..\vendor\Popcorn\src\Pop\%%i
             )
             echo Complete!
+        ) ELSE (
+            echo Downloaded files could not be found.
         )
     )
 )
