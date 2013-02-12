43b3a97b4160d724b25f4ab44d89210748352bf6
Nick Sagona <nick@moc10media.com>
2013-02-11 21:46:06 -0600

    Tweak/update CLI scripts

--- script/pop.bat
+++ script/pop.bat
@@ -3,6 +3,8 @@ REM
 REM Popcorn Micro-Framework Windows CLI script
 REM
 
+SETLOCAL ENABLEDELAYEDEXPANSION
+
 SET TAR=
 SET ZIP=
 SET EXT=
@@ -10,10 +12,10 @@ SET CMD=
 FOR /f "delims=" %%i IN ('where tar') DO SET TAR=%%i
 FOR /f "delims=" %%i IN ('where zip') DO SET ZIP=%%i
 
-IF "%TAR" == "" (
+IF NOT "!TAR!" == "" (
     SET EXT=.tar.gz
-    SET CMD=tar -C ../vendor/Popcorn/src/Pop -xpf
-) ELSE IF NOT "%ZIP" == "" (
+    SET CMD=tar -C ../vendor/Popcorn/src/Pop -xpzf
+) ELSE IF NOT "!ZIP!" == "" (
     SET EXT=.zip
     SET CMD=unzip -q -d ../vendor/Popcorn/src/Pop
 ) ELSE (
@@ -22,21 +24,21 @@ IF "%TAR" == "" (
 )
 
 SET SCRIPT_DIR=%~dp0
-php %SCRIPT_DIR%pop.php %EXT% %*
+php %SCRIPT_DIR%pop.php !EXT! %*
 
 IF "%1" == "install" (
     IF NOT "%2" == "" (
         IF "%2" == "all" (
-            SET TESTFILE=..\vendor\Popcorn\src\Pop\Archive%EXT%
+            SET TESTFILE=..\vendor\Popcorn\src\Pop\Archive!EXT!
         ) ELSE (
             SET TEMPFILE=%2%
-            SET TESTFILE=..\vendor\Popcorn\src\Pop\%TEMPFILE%%EXT%
+            SET TESTFILE=..\vendor\Popcorn\src\Pop\!TEMPFILE!!EXT!
         )
 
-        IF EXIST "%TESTFILE%" (
-            FOR /f "delims=" %%i IN ('dir /B ..\vendor\Popcorn\src\Pop\*%EXT%') DO (
+        IF EXIST "!TESTFILE!" (
+            FOR /f "delims=" %%i IN ('dir /B ..\vendor\Popcorn\src\Pop\*!EXT!') DO (
                 echo Unpacking %%i...
-                %CMD% ../vendor/Popcorn/src/Pop/%%i
+                !CMD! ../vendor/Popcorn/src/Pop/%%i
                 del ..\vendor\Popcorn\src\Pop\%%i
             )
             echo Complete!
@@ -45,3 +47,5 @@ IF "%1" == "install" (
         )
     )
 )
+
+ENDLOCAL
